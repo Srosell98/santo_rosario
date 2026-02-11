e1b8752ea6598c7ad59f6ed5d7f4b70b64473670
Ainencaitar <srosell98@gmail.com>
2026-02-11 19:57:03 +0100

    Initial Commit

---
+++ Rosario/RosarioApp.swift
@@ -0,0 +1,17 @@
+//
+//  RosarioApp.swift
+//  Rosario
+//
+//  Created by Santiago Rosell on 11/2/26.
+//
+
+import SwiftUI
+
+@main
+struct RosarioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
